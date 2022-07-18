6fe998cf46b90aa31cd30273245d305d4ec962d1
honey1510 <98910916+honey1510@users.noreply.github.com>
2022-07-18 04:05:13 -0400

    Firebase Connection and Login_Signup Functionality

--- app/src/main/java/com/example/gameon/LoginScreen.java
+++ app/src/main/java/com/example/gameon/LoginScreen.java
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginScreen extends AppCompatActivity {
     private Button login;
     private TextView signup;
+
+    private EditText email_id, pass_word;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +42,55 @@ public class LoginScreen extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        //LOGIN FUNCTIONALITY
+        email_id=findViewById(R.id.editTextTextEmailAddress);
+        pass_word=findViewById(R.id.editTextTextPassword);
+        mAuth=FirebaseAuth.getInstance();
+
+        login.setOnClickListener(v -> {
+            String email= email_id.getText().toString().trim();
+            String password=pass_word.getText().toString().trim();
+            if(email.isEmpty())
+            {
+                email_id.setError("Email is empty");
+                email_id.requestFocus();
+                return;
+            }
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+            {
+                email_id.setError("Enter the valid email");
+                email_id.requestFocus();
+                return;
+            }
+            if(password.isEmpty())
+            {
+                pass_word.setError("Password is empty");
+                pass_word.requestFocus();
+                return;
+            }
+            if(password.length()<6)
+            {
+                pass_word.setError("Length of password is more than 6");
+                pass_word.requestFocus();
+                return;
+            }
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                if(task.isSuccessful())
+                {
+                    Intent i = new Intent(LoginScreen.this,HomeScreen.class);
+                    startActivity(i);
+                }
+                else
+                {
+                    Toast.makeText(LoginScreen.this,
+                            "Please Check Your login Credentials",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            });
+        });
+
+
     }
 }
--- app/src/main/java/com/example/gameon/SignUpPage.java
+++ app/src/main/java/com/example/gameon/SignUpPage.java
@@ -1,31 +1,35 @@
 package com.example.gameon;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpPage extends AppCompatActivity {
     private Button signup;
     private TextView login;
 
+    EditText user_name, email_id, pass_word, cnf_pass;
+
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
-
-        signup = findViewById(R.id.buttonSignUp);
-
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(SignUpPage.this,HomeScreen.class);
-                startActivity(i);
-            }
-        });
 
         login = findViewById(R.id.login);
 
@@ -36,5 +40,82 @@ public class SignUpPage extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
+
+        //SIGN UP FEATURE
+
+        user_name=findViewById(R.id.editTextTextPersonName);
+        pass_word=findViewById(R.id.editTextPassword);
+        cnf_pass=findViewById(R.id.editTextConfirmPassword);
+        email_id=findViewById(R.id.editTextEmail);
+        signup = findViewById(R.id.buttonSignUp);
+
+        mAuth=FirebaseAuth.getInstance();
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = user_name.getText().toString().trim();
+                String email = email_id.getText().toString().trim();
+                String password= pass_word.getText().toString().trim();
+                String cnfpassword= cnf_pass.getText().toString().trim();
+                if(name.isEmpty())
+                {
+                    user_name.setError("Name is empty");
+                    user_name.requestFocus();
+                    return;
+                }
+                if(email.isEmpty())
+                {
+                    email_id.setError("Email is empty");
+                    email_id.requestFocus();
+                    return;
+                }
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    email_id.setError("Enter the valid email address");
+                    email_id.requestFocus();
+                    return;
+                }
+                if(password.isEmpty())
+                {
+                    pass_word.setError("Enter the password");
+                    pass_word.requestFocus();
+                    return;
+                }
+                if(password.length()<6)
+                {
+                    pass_word.setError("Length of the password should be more than 6");
+                    pass_word.requestFocus();
+                    return;
+                }
+                if(!password.equals(cnfpassword))
+                {
+                    cnf_pass.setError("Password and Confirm Password Doesnt match");
+                    cnf_pass.requestFocus();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(SignUpPage.this,"You are successfully Registered! Please Login", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(SignUpPage.this,LoginScreen.class);
+                            startActivity(i);
+                        }
+                        else
+                        {
+                            Toast.makeText(SignUpPage.this,"You are not Registered! Try again",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
     }
 }
