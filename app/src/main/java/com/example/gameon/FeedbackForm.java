e1cb3a9af0e6ae49e6e348a1ec11f5915e8610c1
honey1510 <98910916+honey1510@users.noreply.github.com>
2022-10-12 23:40:25 -0400

    Feedback Form Added

---
+++ app/src/main/java/com/example/gameon/FeedbackForm.java
@@ -0,0 +1,80 @@
+package com.example.gameon;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FeedbackForm extends AppCompatActivity {
+    public Button submit;
+
+    private EditText emailAddress ;
+    private EditText name ;
+    private EditText feedback ;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback_form);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        submit = findViewById(R.id.btnSubmit);
+        emailAddress = findViewById(R.id.emailId);
+        name = findViewById(R.id.nameField);
+        feedback = findViewById(R.id.feedback);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Map<String, Object> feedbackData = new HashMap<>();
+                feedbackData.put("email", emailAddress.getText().toString());
+                feedbackData.put("name", name.getText().toString());
+                feedbackData.put("feedback", feedback.getText().toString());
+
+                db.collection("feedbacks").add(feedbackData).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        // after the data addition is successful
+                        // we are displaying a success toast message.
+                        Toast.makeText(FeedbackForm.this, "Your Feedback has been added !", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // this method is called when the data addition process is failed.
+                        // displaying a toast message when data addition is failed.
+                        Toast.makeText(FeedbackForm.this, "Fail to add feedback \n" + e, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+        });
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/gameon/UserProfile.java
+++ app/src/main/java/com/example/gameon/UserProfile.java
@@ -15,6 +15,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class UserProfile extends AppCompatActivity {
     private ImageView back;
     //private Button update;
+    private Button feedback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,12 +23,20 @@ public class UserProfile extends AppCompatActivity {
         back = findViewById(R.id.imageView3);
 
         EditText email  = findViewById(R.id.editTextEmail);
+        feedback = findViewById(R.id.buttonFeedback);
         EditText name  = findViewById(R.id.editTextTextPersonName);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(UserProfile.this,HomeScreen.class);
+                startActivity(i);
+            }
+        });
+        feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(UserProfile.this,FeedbackForm.class);
                 startActivity(i);
             }
         });
